0942181d20772d98385b3e51947bbf1ac0714487
LeeoBianchi <leobianchi98@gmail.com>
2023-03-15 14:01:47 +0100

    new runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,55 +1,16 @@
 using Test
+using MPI
 
-@testset "DMap: Scatter & Gather" begin
-    cm = @cmd "mpirun -n 3 julia test_MPI_map.jl"
-    res = run(cm)
-    @test success(res)
-end
 
-@testset "DMap: Algebra" begin
-    cm = @cmd "mpirun -n 3 julia test_MPI_alg_map.jl"
-    res = run(cm)
-    @test success(res)
-end
+testdir = @__DIR__
+istest(f) = endswith(f, ".jl") && startswith(f, "test_")
+testfiles = sort(filter(istest, readdir(testdir)))
+nprocs = clamp(Sys.CPU_THREADS, 2, 4)
 
-@testset "DAlm: Scatter & Gather" begin
-    cm = @cmd "mpirun -n 3 julia test_MPI_alm.jl"
-    res = run(cm)
-    @test success(res)
-end
-
-@testset "DAlm: Algebra" begin
-    cm = @cmd "mpirun -n 3 julia test_MPI_alg_alm.jl"
-    res = run(cm)
-    @test success(res)
-end
-
-@testset "Alm-space parallel dot product" begin
-    cm = @cmd "mpirun -n 3 julia test_MPI_dot.jl"
-    res = run(cm)
-    @test success(res)
-end
-
-@testset "SHT: alm2map direction" begin
-    cm = @cmd "mpirun -n 3 julia test_MPI_a2m.jl"
-    res = run(cm)
-    @test success(res)
-end
-
-@testset "SHT: adjoint direction" begin
-    cm = @cmd "mpirun -n 3 julia test_MPI_adj_a2m.jl"
-    res = run(cm)
-    @test success(res)
-end
-
-@testset "Power spectrum functions: alm2cl & synalm!" begin
-    cm = @cmd "mpirun -n 3 julia test_MPI_cl.jl"
-    res = run(cm)
-    @test success(res)
-end
-
-@testset "Map-space array scattering" begin
-    cm = @cmd "mpirun -n 3 julia test_MPI_arr.jl"
-    res = run(cm)
-    @test success(res)
+@testset "$f" for f in testfiles
+    mpiexec() do mpirun
+        cmd(n) = `$mpirun -n $n $(Base.julia_cmd()) --startup-file=no $(joinpath(testdir, f))`
+        res = run(cmd(nprocs))
+        @test success(res)
+    end
 end
